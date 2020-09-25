16d0bbcfdc617e75a6190476997c5a76c9f4ed8d
sza95 <sza95@outlook.com>
2020-09-25 13:07:53 -0500

    Fixed problem with the footer overlapping video list, made video list a scrollable list to reduce page size and other some minor adjustments and fixes

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,13 +1,17 @@
-</body>
-
-<footer>
-  <div class="card text-center">
-    <div class="card-body">
-      <h5 class="card-title">Special title treatment</h5>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
+<div class="row">
+  <div class="col-12">
+    <footer>
+      <div class="card text-center">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </footer>
   </div>
-</footer>
+</div>
+
+</body>
 
 </html>
