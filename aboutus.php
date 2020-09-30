df182bdb8cf1dd33de8a5842e708221e7de719f4
sza95 <sza95@outlook.com>
2020-09-30 11:24:38 -0500

    Removed dark mode button, style and js class since it wasn't working properly, added about us and contact pages, removed gallery.js file and added form.js file to work along with contact page

--- aboutus.php
+++ aboutus.php
@@ -1,3 +1,17 @@
 <?php include 'layouts/header.php'; ?>
-
+<div class="container">
+    <h3 class="text-center">Sobre la Pagina</h3>
+    <br>
+    <p class="text-justify">
+        Esta pagina fue creada para evidenciar los conocimientos adquiridos en la clase de Nuevas Tecnologías de Programación
+        en donde, y hasta el día de hoy, hemos visto el tema de control de versiones con GitHub, aquí hemos aprendido a manejar este tema
+        que nos ha permitido tener un control y seguimiento de cada cambio que se hace a un proyecto y su código.
+        <br><br>
+        En este caso la pagina que se debe de crear debe de tener cierta similitud a YouTube y en 5 commits, con cualquier servicio de
+        control de versiones, se debe de evidenciar el progreso de dicha página aplicando el conocimiento adquirido en clase.
+        <br><br>
+        Gracias al profesor Carlos Jaramillo por su tiempo, dedicación y pasión al momento de enseñar, gracias a todo esto este tema ha sido
+        fácil de entender, aprender y aplicar.
+    </p>
+</div>
 <?php include 'layouts/footer.php'; ?>
--- layouts/footer.php
+++ layouts/footer.php
@@ -11,10 +11,11 @@
   </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
-<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//geodata.solutions/includes/countrystatecity.js"></script>
 
 </body>
 
